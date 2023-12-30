9fc44d2db3717b02f038543c388afef203526a63
Sherif Abdelmoatty <sherif.ahmed990@gmail.com>
2023-12-30 13:44:06 +0300

    remove Candide account

--- src/account/Candide/types.ts
+++
@@ -1,9 +0,0 @@
-import { BigNumberish, BytesLike } from "ethers";
-import type { Operation } from "../../types";
-
-export interface MetaTransaction {
-    to: string;
-    value: BigNumberish;
-    data: BytesLike;
-    operation: Operation;
-}
