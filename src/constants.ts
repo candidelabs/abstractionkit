e7fd8aa99d0f7e9cece35ca10b9ffdd02031269f
Sherif Abdelmoatty <sherif.ahmed990@gmail.com>
2024-01-30 14:49:21 +0300

    remove UserOperationEmptyValues

--- src/constants.ts
+++ src/constants.ts
@@ -2,20 +2,6 @@ import {type UserOperation } from "./types";
 
 export const ZeroAddress = "0x0000000000000000000000000000000000000000";
 
-export const UserOperationEmptyValues: UserOperation = {
-	sender: ZeroAddress,
-	nonce: 0n,
-	initCode: "0x",
-	callData: "0x",
-	callGasLimit: 0n,
-	verificationGasLimit: 0n,
-	preVerificationGas: 0n,
-	maxFeePerGas: 0n,
-	maxPriorityFeePerGas: 0n,
-	paymasterAndData: "0x",
-	signature: "0x",
-};
-
 export const UserOperationDummyValues: UserOperation = {
 	//dummy values for somewhat accurate gas estimation
 	sender: ZeroAddress,
