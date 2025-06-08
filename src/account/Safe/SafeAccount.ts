59d120d6396ad23e7263b59a1b2ac347a09996e2
Sherif Abdelmoatty <sherif.ahmed990@gmail.com>
2025-06-08 17:47:35 +0300

    Bump version

--- src/account/Safe/SafeAccount.ts
+++ src/account/Safe/SafeAccount.ts
@@ -2917,7 +2917,7 @@ function generateOnChainIdentifier(
     project: string,
     platform: "Web" | "Mobile" | "Safe App" | "Widget" = "Web",
     tool: string = "abstractionkit",
-    toolVersion: string = "0.2.21"
+    toolVersion: string = "0.2.22"
 ): string {
     const identifierPrefix = '5afe'; // Safe identifier prefix
     const identifierVersion = '00'; // First version
